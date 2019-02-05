edd060a9e9d8bb74c5a650ebce94fa2dd6427104
mcellocello <37343285+mcellocello@users.noreply.github.com>
2019-02-05 17:04:44 +0700

    Update imej.py

--- imej.py
+++ imej.py
@@ -1,11 +1,20 @@
 import os
 from PIL import Image,ImageEnhance
-# skullraw=Image.open('skull.jpg')
-# skullraw=skullraw.resize((960,960),Image.ANTIALIAS)
-# skullraw=skullraw.convert('L')
-# scanraw=Image.open('scan.png')
-# scanraw=scanraw.resize((960,960),Image.ANTIALIAS)
-# skullraw.paste(scanraw,(0,0),scanraw)
-print(os.listdir('/sdcard/dcim/100PINT/Pins/'))
-# skullraw.show()
-# skullraw.save('res.jpg')
+listdir=os.listdir('/sdcard/dcim/100PINT/Pins/')
+# listdir=os.listdir()
+num=0
+lens=len(listdir)
+for i in listdir:
+    print(str(num)+'. '+i)
+    num+=1
+
+print('\nPilih gambar yang mana ? '+'0 - '+str(lens-1))
+x=int(input('Nomor = '))
+skullraw=Image.open(listdir[x])
+skullraw=skullraw.resize((960,960),Image.ANTIALIAS)
+skullraw=skullraw.convert('L')
+scanraw=Image.open('scan.png')
+scanraw=scanraw.resize((960,960),Image.ANTIALIAS)
+skullraw.paste(scanraw,(0,0),scanraw)
+skullraw.show()
+skullraw.save('/sdcard/res.jpg')
