1c8428fa74142d5db25e8bbbfc70551f7c9c69f9
mcellocello <37343285+mcellocello@users.noreply.github.com>
2019-02-05 16:40:47 +0700

    Add files via upload

---
+++ imej.py
@@ -0,0 +1,11 @@
+import os
+from PIL import Image,ImageEnhance
+# skullraw=Image.open('skull.jpg')
+# skullraw=skullraw.resize((960,960),Image.ANTIALIAS)
+# skullraw=skullraw.convert('L')
+# scanraw=Image.open('scan.png')
+# scanraw=scanraw.resize((960,960),Image.ANTIALIAS)
+# skullraw.paste(scanraw,(0,0),scanraw)
+print(os.listdir('/sdcard/dcim/100PINT/Pins/'))
+# skullraw.show()
+# skullraw.save('res.jpg')
