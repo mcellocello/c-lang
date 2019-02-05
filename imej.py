64839870ab8c6aca72d559b2d53c5ad83b2eece4
mcellocello <37343285+mcellocello@users.noreply.github.com>
2019-02-05 17:08:14 +0700

    Update imej.py

--- imej.py
+++ imej.py
@@ -10,11 +10,11 @@ for i in listdir:
 
 print('\nPilih gambar yang mana ? '+'0 - '+str(lens-1))
 x=int(input('Nomor = '))
-skullraw=Image.open(listdir[x])
+skullraw=Image.open('/sdcard/dcim/100PINT/Pins/'+listdir[x])
 skullraw=skullraw.resize((960,960),Image.ANTIALIAS)
 skullraw=skullraw.convert('L')
 scanraw=Image.open('scan.png')
 scanraw=scanraw.resize((960,960),Image.ANTIALIAS)
 skullraw.paste(scanraw,(0,0),scanraw)
 skullraw.show()
-skullraw.save('/sdcard/res.jpg')
+skullraw.save('res.jpg')
